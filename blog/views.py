10d031383bb140ead917d17a8e555722ed8ba0ac
iulianstave <iulian.staverescu@gmail.com>
2019-11-21 12:26:24 +0200

    Add bootstrap classes

--- blog/views.py
+++ blog/views.py
@@ -3,23 +3,24 @@ from django.shortcuts import render
 
 posts = [
     {
-        'author': 'Stave',
-        'title': 'Blog post',
-        'content':'First post conent',
+        'author': 'Iulian Stave',
+        'title': 'First blog post',
+        'content':'First post content',
         'date_posted': 'August 27, 2018'
     },
     {
-        'author': 'John',
-        'title': 'Blog post two',
-        'content':'Second post conent',
-        'date_posted': 'August 27, 2018'
+        'author': 'John Smith',
+        'title': 'Second post',
+        'content':'Second post content',
+        'date_posted': 'August 3, 2019'
     }
 ]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': posts,
+        'title': 'Blog start',
     }
     return render(request, 'blog/home.html', context)
 
