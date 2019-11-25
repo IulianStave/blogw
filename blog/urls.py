967c48e1f2f126225a892074e83da8079b78d216
iulianstave <iulian.staverescu@gmail.com>
2019-11-25 17:43:20 +0200

    Add update post and User test mixin

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
 from . import views
 
 
@@ -13,8 +13,8 @@ urlpatterns = [
     path('', PostListView.as_view(), name = 'blog-home'),
    # path('post/<int:pk>/', PostDetailView.as_view(), name = 'blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name = 'post-detail'),
-    path('post/new/', PostCreateView.as_view(), name = 'post-create'),#model_form.html
-
+    path('post/new/', PostCreateView.as_view(), name = 'post-create'),#model_form.html,
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name = 'post-update'),
     path('about/', views.about, name = 'blog-about'),
     
 ]
