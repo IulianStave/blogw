e1e3b0274a77ddbe4663e028a65aac24c7d09c11
iulianstave <iulian.staverescu@gmail.com>
2019-11-25 14:25:39 +0200

    Add user and profile update

--- users/forms.py
+++ users/forms.py
@@ -7,6 +7,7 @@ pip3 install django-crispy-forms
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -16,3 +17,16 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+# form that will update the user model
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email'] 
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
